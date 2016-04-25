b9bcb9b68d9b2a228971ab6a0e5b7cf1bb57768c
Ryan Whitehurst <ryan@ryanwhitehurst.com>
2016-04-25 13:05:08 -0700

    Remove test for jmxtrans::merge_notundef
    `rspec-puppet` can't test functions defined in the puppet language.

--- spec/functions/merge_notundef_spec.rb
+++ spec/functions/merge_notundef_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe 'jmxtrans::merge_notundef' do
-  it { is_expected.to run.with_params({ 'a' => 1 }, { 'b' => 2 }).and_return({ 'a' => 1, 'b' => 2}) }
-end
+# describe 'jmxtrans::merge_notundef' do
+#   it { is_expected.to run.with_params({ 'a' => 1 }, { 'b' => 2 }).and_return({ 'a' => 1, 'b' => 2}) }
+# end
